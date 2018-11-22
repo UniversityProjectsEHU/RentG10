62445d63b7ab5c8f1eefd64b7e86bf95a88fa7e9
Sergio <Sergio@10.111.170.127>
2018-11-22 18:05:41 +0100

    Ampliación de jsLogin(Validación e tiempo real)

--- public_html/javascript/jsLogin.js
+++ public_html/javascript/jsLogin.js
@@ -1,14 +1,20 @@
 
 window.addEventListener('load', inicio, false);
-
+var correo,contra;
 function inicio() {
     document.getElementById('botonenviar').addEventListener('click', guardar, false);
-    document.getElementById('cerrarsesion').addEventListener('click', borrardatos, false);
+    document.getElementById('cerrarsesion').addEventListener('click', borrardatos, false); 
     /*Obtener datos almacenados */
     var nombre = localStorage.getItem("Nombre");
     /*Mostrar datos almacenados*/
     var nombreusuario = localStorage.getItem("Nombre");
     document.getElementById("nombree").innerHTML = nombreusuario;
+    //Validación en tiempo real
+    usuario=document.getElementById("email");
+    contra=document.getElementById("contrasena");
+    usuario.addEventListener("input",validacion);
+    contra.addEventListener("input",validacion);
+    validacion();
 }
 
 function guardar(evt) {
@@ -27,3 +33,24 @@ function borrardatos (evt) {
     document.getElementById("nombree").innerHTML = nombreusuario;
 }
 
+function validacion(){
+    
+    if(usuario.value===""){
+        usuario.setCustomValidity("Inserte su nombre o su apellido");
+        usuario.style.background ="red" ;
+    }
+    else{
+        usuario.setCustomValidity("");
+        usuario.style.background = "green";
+    }
+    
+    if(contra.value===""){
+        contra.setCustomValidity("Inserte su contraseña");
+        contra.style.background="red";
+    }
+    else{
+        contra.setCustomValidity("");
+        contra.style.background = "green";
+    }
+
+}
