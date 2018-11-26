96083567d203ba2c4ddf7bef3257bbf4f6891688
serna <serna@DESKTOP-1656E2S>
2018-11-26 22:49:14 +0000

    Consulta Usuario en funcionamiento

--- public_html/javascript/jsConsultaAdmin.js
+++ public_html/javascript/jsConsultaAdmin.js
@@ -1,35 +1,37 @@
 window.addEventListener('load', inicio, false);
 var puede;
 function inicio() {
-    document.getElementById('cerrarsesion').addEventListener('click', borrardatos, false); 
+    document.getElementById('cerrarsesion').addEventListener('click', borrardatos, false);
     /*Obtener datos almacenados */
     var nombre = localStorage.getItem("Nombre");
     /*Mostrar datos almacenados*/
     var nombreusuario1 = localStorage.getItem("Nombre");
     var nombreusuario = localStorage.getItem("Nombre").toString();
-    var emailusuario = localStorage.getItem("Email").toString(); 
+    var emailusuario = localStorage.getItem("Email").toString();
     document.getElementById("nombree").innerHTML = nombreusuario1;
-    
-    puede=false;
-    
-    if(nombreusuario==="admin1" && emailusuario==="admin@gmail.com"){
-       puede=true;
+
+//    if ( localStorage.getItem("Nombre") === null) {
+//        location.href = "../html/pantallaInicial.html";
+//    }
+    puede = false;
+    if (nombreusuario === "admin1" && emailusuario === "admin@gmail.com") {
+        puede = true;
     }
-    if(nombreusuario==="admin2" && emailusuario==="admin2@gmail.com"){
-       puede=true;
+    if (nombreusuario === "admin2" && emailusuario === "admin2@gmail.com") {
+        puede = true;
     }
-    if(puede===false){
-      location.href ="../html/pantallaInicial.html"; 
-    } 
+    if (puede === false) {
+        location.href = "../html/pantallaInicial.html";
+    }
 }
 
 //Cierra la sesion y te manda a la pantalla inicial
-function borrardatos (evt) {
+function borrardatos(evt) {
     localStorage.clear();
     /*Mostrar datos almacenados*/
     var nombreusuario = localStorage.getItem("Nombre");
     document.getElementById("nombree").innerHTML = nombreusuario;
-    location.href ="../html/pantallaInicial.html";
+    location.href = "../html/pantallaInicial.html";
 }
 
 
--- public_html/javascript/jsConsultaUsuario.js
+++ public_html/javascript/jsConsultaUsuario.js
@@ -1,20 +1,142 @@
+
 window.addEventListener('load', inicio, false);
 
-
+var correo, contra;
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+var bd;
+var dayInMillis = 24 * 3600000;
 function inicio() {
     alert("estoy en inicio");
-    document.getElementById('cerrarsesion').addEventListener('click', borrardatos, false); 
+//    document.getElementById('botonlogin').addEventListener('click', guardar, false);
+    document.getElementById('cerrarsesion').addEventListener('click', borrardatos, false);
     /*Obtener datos almacenados */
     var nombre = localStorage.getItem("Nombre");
     /*Mostrar datos almacenados*/
     var nombreusuario = localStorage.getItem("Nombre");
     document.getElementById("nombree").innerHTML = nombreusuario;
+
+    document.getElementById('botonmostrar').addEventListener('click', comprobar, false);
+    var solicitud = indexedDB.open("basededatos");
+    solicitud.addEventListener("success", comenzar);
+    solicitud.addEventListener("upgradeneeded", crearbd);
+
+    var button = document.getElementById("botonenviar");
+    button.addEventListener("click", comprobar);
+
 }
-function borrardatos (evt) {
+function comenzar(evento) {
+    alert("sacses");
+    bd = evento.target.result;
+}
+
+function borrardatos(evt) {
     localStorage.clear();
     /*Mostrar datos almacenados*/
     var nombreusuario = localStorage.getItem("Nombre");
     document.getElementById("nombree").innerHTML = nombreusuario;
-     location.href ="../html/pantallaInicial.html";
+    location.href = "../html/pantallaInicial.html";
 }
 
+function crearbd(evento) {
+    alert("Estoy creando");
+    bd = evento.target.result;
+    var basededatos = evento.target.result;
+    var clientes = basededatos.createObjectStore("clientes", {keyPath: "correo"});
+    clientes.createIndex('by_nombre', 'nombre', {unique: false});
+    clientes.createIndex('by_contra', 'contra', {unique: false});
+    clientes.createIndex('by_DNI', 'dni', {unique: true});
+    clientes.createIndex('by_tel', 'telefono', {unique: true});
+    clientes.add({correo: "admin@gmail.com", nombre: "admin1", contra: "admin", dni: "123456789", telefono: "111222333"});
+    clientes.add({correo: "admin2@gmail.com", nombre: "admin2", contra: "admin2", dni: "123456787", telefono: "111222334"});
+
+
+
+    var coches = basededatos.createObjectStore("coches", {keyPath: "matricula"});
+    coches.createIndex('by_Marca', 'marca', {unique: false});
+    coches.createIndex('by_caract', 'caracteristica', {unique: false});
+    coches.add({matricula: "abc-789", marca: "Mercedes", caracteristica: "mediano"});
+    coches.add({matricula: "abc-123", marca: "Fiat", caracteristica: "grande"});
+    coches.add({matricula: "cba-547", marca: "Jaguar", caracteristica: "pequeño"});
+
+
+    var reservas = basededatos.createObjectStore("reservas", {keyPath: "id", autoIncrement: true});
+    reservas.createIndex('by_Matricula', 'matricula', {unique: false});
+    reservas.createIndex('by_correo', 'correo', {unique: false});
+    reservas.createIndex('by_fi', 'fi', {unique: false});
+    reservas.createIndex('by_hi', 'hi', {unique: false});
+    reservas.createIndex('by_ff', 'ff', {unique: false});
+    reservas.createIndex('by_hf', 'hf', {unique: false});
+    reservas.createIndex('by_lugar', 'lugar', {unique: false});
+
+
+}
+function comprobar() {
+    alert("Etoi comprobando");
+    var transaccion = bd.transaction(["reservas"]);
+    alert("Estoy en bd");
+    var almacenclientes = transaccion.objectStore("reservas");
+    var puntero = almacenclientes.openCursor();
+    puntero.addEventListener("success", mostrarlista);
+}
+function comprueba(event) {
+    alert("comprueba");
+    var puntero = event.target.result;
+    if (puntero) {
+        if (puntero.value.correo.toString() === document.getElementById('email').value) {
+            alert("Bien");
+            if (puntero.value.contra.toString() === document.getElementById('contrasena').value) {
+                alert("Contra tambien bien");
+                guardar(puntero.value.nombre.toString(), document.getElementById('email').value.toString());
+                alert("Estas Logeado");
+            }
+        } else {
+            alert("Lo sentimos tu usuario y contraseña no coinciden");
+        }
+
+    }
+    puntero.continue();
+}
+
+function mostrarlista(evento) {
+    alert("Estoy en mostrar lista");
+    var puntero = evento.target.result;
+    var fechaapuntada = document.getElementById("fecha").value;
+    var fechaformulario = new Date(fechaapuntada);
+    
+    
+    var fechabase = puntero.value.fi;
+    var fechasin = fechabase.split("-");
+    var fechafinal = fechasin[0]+","+fechasin[1]+","+fechasin[2];
+    var d = new Date(fechafinal);
+    alert(fechaformulario.getTime() + "ESTA ES LA DEL FOR");
+    alert(d.getTime() + "ESTA ES LA DE BD");
+ 
+    if (puntero) {
+        if (puntero.value.correo === localStorage.getItem("Email")) {
+             alert("entro1");
+            if (d.getTime()>fechaformulario.getTime()){
+                alert("entro2");
+                document.getElementById('cajadatos').innerHTML += "<div>" + puntero.value.id + 
+                        " / " + puntero.value.matricula + " / " + puntero.value.correo + 
+                        " / " + puntero.value.fi + " / " + puntero.value.hi +
+                        " / " + puntero.value.ff + " / " + puntero.value.hf +
+                        " / " + puntero.value.lugar +"</div>";
+            }
+        }
+        puntero.continue();
+    }
+}
+
+
+function compareDates(date1, date2) {
+    // Nos quedamos con los días completos pasados desde el 1 de enero de 1970
+    let days1 = Math.floor(date1.getTime() / dayInMillis);
+    let days2 = Math.floor(date2.getTime() / dayInMillis);
+    // comparamos los días
+    if (days1 > days2) {
+        return 1;
+    } else if (days1 < days2) {
+        return -1;
+    }
+    return 0;
+}
